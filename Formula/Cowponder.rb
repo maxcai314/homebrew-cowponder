e07839658d6679f6fbd594d3442f487a88712532
Max Cai <maxcai.paloalto@gmail.com>
2023-09-18 23:12:38 -0700

    enough ruby for today.

--- Formula/Cowponder.rb
+++ Formula/Cowponder.rb
@@ -2,7 +2,7 @@ class Cowponder < Formula
   desc "A simple terminal command that displays randomly selected philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://xz.ax/cowponder-homebrew-v0.0.1.tar.gz"
-  sha256 "checksum"
+#  sha256 "checksum" # this is a later issue
 
   # Additional options and dependencies can be specified here
   depends_on "python@3"
