9613ad96546ca9694da686dbacfe5f5c76c58da1
Max Cai <maxcai.paloalto@gmail.com>
2023-09-18 22:40:56 -0700

    write a dummy .rb formula

---
+++ Formula/cowponder.rb
@@ -0,0 +1,16 @@
+class Cowponder < Formula
+  desc "Description of your package"
+  homepage "https://example.com"
+  url "https://example.com/your-package/archive/v1.0.0.tar.gz"
+  sha256 "checksum"
+
+  # Additional options and dependencies can be specified here
+
+  def install
+    # Build and install your package here
+  end
+
+  test do
+    # Add some test cases here if applicable
+  end
+end
