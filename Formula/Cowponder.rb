2bf0b7f758dcf4349a161e456784b85e7021c6d0
Max Cai <maxster@xz.ax>
2023-09-19 14:24:09 -0700

    add the make build dependency for now, cus i might use it for the install script

--- Formula/Cowponder.rb
+++ Formula/Cowponder.rb
@@ -5,6 +5,7 @@ class Cowponder < Formula
 #  sha256 "checksum" # this is a later issue
 
   # Additional options and dependencies can be specified here
+  depends_on "make" => :build
   depends_on "python@3"
   depends_on "cowsay"
 
