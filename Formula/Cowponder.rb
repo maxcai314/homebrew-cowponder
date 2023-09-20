adf1da7c646fad200e5d961eb82a8a3d38ccf68c
Max Cai <maxster@xz.ax>
2023-09-19 19:19:07 -0700

    modify description to meet the stupid grammar checks

--- Formula/Cowponder.rb
+++ Formula/Cowponder.rb
@@ -1,11 +1,11 @@
 class Cowponder < Formula
-  desc "A simple terminal command that displays randomly selected philosophical thoughts from a cow"
+  desc "Simple terminal command to display random philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://xz.ax/cowponder-homebrew-v0.0.1.tar.gz"
   sha256 "239dc0b21c11407c7435e7f25afe84659c4ad166fd15e57ef7ac6a6a74a623ca"
 
-  depends_on "python@3"
   depends_on "cowsay"
+  depends_on "python@3"
 
   def install
     bin.install "ponder"
