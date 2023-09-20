f46da9f58f15c00eb1854ae08214c2e59d4d4288
Max Cai <maxster@xz.ax>
2023-09-19 19:12:43 -0700

    add a sha 256 checksum

--- Formula/Cowponder.rb
+++ Formula/Cowponder.rb
@@ -2,9 +2,8 @@ class Cowponder < Formula
   desc "A simple terminal command that displays randomly selected philosophical thoughts from a cow"
   homepage "https://github.com/maxcai314/homebrew-cowponder"
   url "https://xz.ax/cowponder-homebrew-v0.0.1.tar.gz"
-#  sha256 "checksum" # this is a later issue
+  sha256 "239dc0b21c11407c7435e7f25afe84659c4ad166fd15e57ef7ac6a6a74a623ca"
 
-  # Additional options and dependencies can be specified here
   depends_on "python@3"
   depends_on "cowsay"
 
